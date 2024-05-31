f3d90b6a5c3f22909061306cc86a495210b94b20
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-05-31 19:52:23 +0900

    add nav hover event and button effect

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 
+const LINK_EFFECT =
+  "relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left";
+
 export default function Header() {
   return (
     <header className="absolute top-4 left-1/2 translate-x-[-50%]">
@@ -8,45 +11,39 @@ export default function Header() {
         <ul className="flex items-center gap-8">
           {/* header__left */}
           <li className="text-lg font-bold">
-            <Link
-              href="#"
-              className="group relative grid place-items-center overflow-hidden
-              before:content-[''] before:absolute before:left-0 before:bottom-0 before:w-full before:h-[2px] before:bg-current before:transform before:scale-x-0
-              before:transition-transform before:duration-300 before:origin-right before:ease-in-out
-            after:content-[attr(data-text)] after:absolute after:top-0 after:h-full after:transform after:translate-x-96 after:transition-transform after:duration-300 after:ease-in-out
-            hover:before:transform hover:before:scale-100 hover:before:origin-left
-            hover:after:transform hover:after:translate-x-0 hover:after:translate-y-0 hover:after:translate-z-0
-            "
-            >
-              <span className="transition-transform duration-300 ease-in-out group-hover:transform group-hover:-translate-x-150">
-                Home
-              </span>
+            <Link href="#" className={LINK_EFFECT}>
+              <span className="">Home</span>
             </Link>
           </li>
           <li className="text-lg font-bold">
-            <Link href="#">
+            <Link href="#" className={LINK_EFFECT}>
               <span>Reviews</span>
             </Link>
           </li>
           <li className="text-lg font-bold">
-            <Link href="#">
+            <Link href="#" className={LINK_EFFECT}>
               <span>Projects</span>
             </Link>
           </li>
           <li className="text-lg font-bold">
-            <Link href="#">
+            <Link href="#" className={LINK_EFFECT}>
               <span>Insights</span>
             </Link>
           </li>
           <li className="text-lg font-bold">
-            <Link href="#">
+            <Link href="#" className={LINK_EFFECT}>
               <span>Faq</span>
             </Link>
           </li>
         </ul>
         <div>
           {/* header__right */}
-          <button className=" text-white">Contact</button>
+          <button className="text-whit relative inline-flex  group">
+            <div className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
+            <span className="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900">
+              Contact
+            </span>
+          </button>
         </div>
       </div>
     </header>
