bd8f55f5de6c444b912ef93eb050d137f0b91b06
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-05-31 18:40:23 +0900

    background animate

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,34 +1,67 @@
 "use client";
+import Image from "next/image";
 import { useEffect, useState } from "react";
+import loadingImage from "../../public/img/loader13.svg";
 
 const loadingBarAnimate = "duration-[5000ms] rotate-90 left-[1000%]";
-const opacityAnimate = "duration-[1000ms] opacity-0";
+const opacityAnimate = "duration-[500ms] opacity-0";
+const boxAnimate = "duration-[1000ms] h-[500px] rounded-[50%]";
+const boxAnimate2 = "border-none";
+const svgAnimate = "duration-[10000ms] opacity-100 rotate-180 rounded-[50%]";
+const svgAnimate2 = "duration-[50000ms] rotate-[360deg] opacity-100";
+const backgroundAnimate =
+  "duration-[2000ms] delay-[2000ms] bg-transparent opacity-50";
 
 export default function Loading() {
   const [barWidth, setBarWidth] = useState(0);
+  const [timeOutState, setTimeOutState] = useState(false);
 
   const callApi = () => {
     setBarWidth((state) => state + 1);
   };
 
   useEffect(() => {
+    let effectTimeout: any;
     const bar = setInterval(() => {
       callApi();
     }, 10);
     if (barWidth >= 100) {
       clearInterval(bar);
+      effectTimeout = setTimeout(() => {
+        setTimeOutState(true);
+      }, 2000);
     }
-    return () => clearInterval(bar);
+
+    return () => {
+      clearInterval(bar), clearTimeout(effectTimeout);
+    };
   }, [barWidth]);
 
   return (
-    <div className="fixed inset-0 h-screen w-full z-[99] bg-[#0b134f] grid place-items-center font-orbitron">
+    <div
+      className={`fixed inset-0 h-screen w-full z-[99] bg-[#0b134f] grid place-items-center font-orbitron
+    ${barWidth === 100 && backgroundAnimate} 
+    
+    `}
+    >
       <div
-        className="relative w-[500px] h-[200px] border-[3px] border-solid border-green-400 border-t-green-700 border-b-green-700 
-      max-[600px]:w-[90vw]"
+        className={`relative w-[500px] h-[200px] border-[3px] border-solid border-green-400 border-t-green-700 border-b-green-700 
+      max-[600px]:w-[90vw] 
+      ${barWidth === 100 && boxAnimate} ${timeOutState && boxAnimate2}
+
+      
+      `}
       >
+        <Image
+          src={loadingImage}
+          alt=""
+          fill={true}
+          className={`absolute top-1/2 left-1/2 ${
+            barWidth === 100 && !timeOutState ? svgAnimate : "opacity-0"
+          } ${timeOutState && svgAnimate2}`}
+        />
         <div
-          className={`relative text-white   p-4 text-xl font-bold flex items-center justify-center max-[355px]:text-sm ${
+          className={`relative text-white p-4 text-xl font-bold flex items-center justify-center max-[355px]:text-sm ${
             barWidth === 100 && opacityAnimate
           }`}
         >
