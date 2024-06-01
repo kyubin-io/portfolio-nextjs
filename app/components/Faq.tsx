c26d256db8ff5ab11f604ef7a596458c33a7a258
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-06-02 00:12:52 +0900

    add questions

--- app/components/Faq.tsx
+++ app/components/Faq.tsx
@@ -6,6 +6,31 @@ const questions = [
     answer:
       "This completely depends on what you need. It takes longer to build a 1000 page megasite than a smaller eight page brochure website.",
   },
+  {
+    question: "What tech do you mainly use in projects?",
+    answer:
+      "I mainly use JavaScript and React. For data management, I use Redux or Context API, and for UI, I prefer CSS and Tailwind CSS.",
+  },
+  {
+    question: "What projects have you collaborated on? Any challenges faced?",
+    answer:
+      "Recently, I worked on a team project in my company. The main challenges were code conflicts and communication gaps. We tackled these by using Gitlab  actively and having weekly meetings to share project progress.",
+  },
+  {
+    question: "Have you refactored or improved your code before?",
+    answer:
+      "Yes, I'm keen on continually improving and refactoring my code. Recently, I refactored the API call part of our project by separating it into Redux middleware for cleaner code.",
+  },
+  {
+    question: "How do you approach learning and growing?",
+    answer:
+      "I believe in continuous learning and growth. I take online courses to learn new technologies and actively participate in developer communities to share knowledge. Also, I learn and grow through project challenges and reflecting on past code for self-improvement.",
+  },
+  {
+    question: "Thoughts on collaborating in team projects?",
+    answer:
+      "Effective communication and mutual respect are crucial for collaborating in team projects. Good communication fosters smooth interaction and problem-solving, while respecting each other's tasks and cooperating are vital for project success.",
+  },
 ];
 
 export default function Faq() {
