d230d47254636519411b11a03e834c211b7b23c8
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-06-02 23:27:31 +0900

    modify skilss

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -10,9 +10,18 @@ export default function Skills() {
       <div className="absolute text-yellow-400 whitespace-nowrap font-roboto font-normal left-0 top-24 animate-moveLeft max-[700px]:text-2xl max-[700px]:top-20">
         WEB DEVELOPMENT&nbsp;&nbsp;&nbsp; Frontend&nbsp;&nbsp;&nbsp;
         Backend&nbsp;&nbsp;&nbsp; API Development&nbsp;&nbsp;&nbsp;
+        RESPONSIVE&nbsp;&nbsp;&nbsp; UI/UX DEsign&nbsp;&nbsp;&nbsp; WEB
+        DEVELOPMENT&nbsp;&nbsp;&nbsp; Frontend&nbsp;&nbsp;&nbsp;
+        Backend&nbsp;&nbsp;&nbsp; API Development&nbsp;&nbsp;&nbsp;
+        RESPONSIVE&nbsp;&nbsp;&nbsp; UI/UX DEsign&nbsp;&nbsp;&nbsp;
       </div>
       {/* skills__text right */}
       <div className="absolute text-yellow-400 whitespace-nowrap font-roboto right-0 top-44 animate-moveRight max-[700px]:text-2xl max-[700px]:top-32">
+        HTML&nbsp;&nbsp;&nbsp; CSS&nbsp;&nbsp;&nbsp;
+        Javascript&nbsp;&nbsp;&nbsp; React.js&nbsp;&nbsp;&nbsp;
+        Next.js&nbsp;&nbsp;&nbsp; Node.js&nbsp;&nbsp;&nbsp;
+        Express.js&nbsp;&nbsp;&nbsp; Tailwind&nbsp;&nbsp;&nbsp;
+        MongoDB&nbsp;&nbsp;&nbsp; PostgreSQL&nbsp;&nbsp;&nbsp;
         HTML&nbsp;&nbsp;&nbsp; CSS&nbsp;&nbsp;&nbsp;
         Javascript&nbsp;&nbsp;&nbsp; React.js&nbsp;&nbsp;&nbsp;
         Next.js&nbsp;&nbsp;&nbsp; Node.js&nbsp;&nbsp;&nbsp;
